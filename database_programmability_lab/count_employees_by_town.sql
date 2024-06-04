c1a11c0bf87645b57adb9582d1cf592fcb61f331
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-06-04 14:58:48 +0300

    Update count_employees_by_town.sql

--- database_programmability_lab/count_employees_by_town.sql
+++ database_programmability_lab/count_employees_by_town.sql
@@ -1,1 +1,23 @@
 
+CREATE OR REPLACE FUNCTION fn_count_employees_by_town
+	(town_name varchar(20))
+RETURNS INT AS
+	
+$$
+	DECLARE 
+		town_count int;
+	BEGIN
+		SELECT
+	COUNT(*) into town_count
+FROM employees as e
+	JOIN addresses as a
+		USING (address_id)
+			JOIN towns as t
+				USING (town_id)
+WHERE t.name = town_name;
+	RETURN town_count;
+	END;
+$$
+LANGUAGE plpgsql ;
+
+SELECT fn_count_employees_by_town('Sofia');
