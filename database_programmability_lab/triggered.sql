822dda03aa3c4c9161b6eb6c0257512d069148f0
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-06-05 11:47:38 +0300

    Create triggered.sql

---
+++ database_programmability_lab/triggered.sql
@@ -0,0 +1,36 @@
+CREATE TABLE deleted_employees(
+	employee_id SERIAL PRIMARY KEY, 
+	first_name varchar(20), 
+	last_name varchar(20), 
+	middle_name varchar(20), 
+	job_title varchar(50), 
+	department_id int, 
+	salary numeric (19,4)
+);
+
+CREATE OR REPLACE FUNCTION backup_fired_emp()
+RETURNS TRIGGER
+AS
+$$
+	BEGIN
+		INSERT INTO deleted_employees(
+			first_name, last_name, middle_name, job_title, deparment_id, salary
+		)
+		VALUES (
+			old.first_name, 
+			old.last_name, 
+			old.middle_name, 
+			old.job_title, 
+			old.department_id, 
+			old.salary
+		);
+		RETURN new;
+	END;
+$$
+LANGUAGE plpgsql
+;
+
+CREATE OR REPLACE trigger backup_employees
+AFTER DELETE on employees
+FOR EACH ROW 
+EXECUTE PROCEDURE backup_fired_emp();
