609b8354feb504b77c38bef37156dc9b1e09d72b
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-06-11 10:48:01 +0300

    Update all_volunteers_in_a_department.sql

--- zoo_exam_lab/all_volunteers_in_a_department.sql
+++ zoo_exam_lab/all_volunteers_in_a_department.sql
@@ -1,1 +1,20 @@
-
+CREATE OR REPLACE FUNCTION fn_get_volunteers_count_from_department(
+	searched_volunteers_department VARCHAR(30)
+) RETURNS int 
+AS
+$$
+	DECLARE 
+	volunteers int;
+BEGIN
+	SELECT 
+		count(*) into volunteers
+	FROM 
+		volunteers as v
+			join volunteers_departments as vd
+				on v.department_id = vd.id
+	WHERE vd.department_name = searched_volunteers_department;
+	RETURN volunteers;
+END;
+$$
+LANGUAGE plpgsql
+;
