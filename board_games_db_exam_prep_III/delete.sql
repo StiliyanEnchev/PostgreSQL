26a6e284ea8f645e70d70ffe4adfb2bb8287a7d9
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-06-12 17:20:10 +0300

    Update delete.sql

--- board_games_db_exam_prep_III/delete.sql
+++ board_games_db_exam_prep_III/delete.sql
@@ -1,1 +1,17 @@
 
+DELETE FROM board_games 
+	where publisher_id IN 
+		(SELECT id 
+		FROM publishers 
+		WHERE address_id in (
+		select id from addresses
+		where town LIKE 'L%'
+		));
+
+DELETE FROM publishers 
+	WHERE address_id in 
+		(select id from addresses 
+			where town like 'L%');
+
+DELETE FROM addresses 
+	where town like 'L%';
